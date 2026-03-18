9975bd136bfe68b61388c966790393970703dec6
Dawid Magnucki <david.magnucki@gmail.com>
2026-03-18 19:32:29 +0100

    scheduler fixed

--- src/main/java/com/recipefinder/backend/scheduler/RecipeScheduler.java
+++ src/main/java/com/recipefinder/backend/scheduler/RecipeScheduler.java
@@ -11,12 +11,21 @@ public class RecipeScheduler {
 
     private final RecipeService recipeService;
 
-    @Scheduled(cron = "0 0 6 * * *")
+    @Scheduled(cron = "0 0 7 * * *")
     public void dailyRecipePlan() {
-        System.out.println("Executing scheduled daily menu generation at 06:00...");
+        System.out.println("Executing scheduled daily menu generation at 07:00...");
 
         recipeService.generateDailyMenu();
 
         System.out.println("Your daily menu has been generated and saved to the database!");
+    }
+
+    @Scheduled(cron = "0 */15 * * * *")
+    public void fetchRandomRecipeEveryFifteenMinutes() {
+        System.out.println("Fetching a random recipe from the scheduler...");
+
+        recipeService.fetchAndSaveRandomRecipe();
+
+        System.out.println("A scheduled random recipe has been fetched and saved.");
     }
 }
